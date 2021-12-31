4621f792cef6a77a3dd4be44d95e965b91cd946e
lionelnhat26 <lionelnhat26@gmail.com>
2021-12-31 21:52:20 +0700

    add validation, error-exception handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+
+        [HttpGet("unauthorised")]
+        public ActionResult GetUnauthorised()
+        {
+            return Unauthorized();
+        }
+    }
+}
